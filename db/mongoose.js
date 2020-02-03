890e68099948976342d30aadc7de663c554e3293
nana <jooankrah@gmail.com>
2020-02-03 14:57:21 +0000

    updated db connection to work with heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/uniapp', {
+mongoose.connect('mongodb://127.0.0.1:27017/uniapp||mongodb://<dbuser>:<dbpassword>@ds061681.mlab.com:61681/heroku_q0q5mnrs', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
