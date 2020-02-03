58a31e420105245f305b92b38af5ad8345ab5878
nana <jooankrah@gmail.com>
2020-02-03 18:07:48 +0000

    Update

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/uniapp'||'mongodb://Jooankrah:Nanakusi1z@ds061681.mlab.com:61681/heroku_q0q5mnrs', {
+const dbUrl = 'mongodb://Jooankrah:Nanakusi1z@ds061681.mlab.com:61681/heroku_q0q5mnrs'
+
+mongoose.connect('mongodb://127.0.0.1:27017/uniapp'|| dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
