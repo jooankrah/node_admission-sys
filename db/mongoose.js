cd9beb65688121ec5c06fb467610344522054b50
nana <jooankrah@gmail.com>
2020-02-03 22:18:34 +0000

    update

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dbUrl = 'mongodb://Jooankrah:Nanakusi1z@ds061681.mlab.com:61681/heroku_q0q5mnrs'
+const dbUrl = 'mongodb://<dbuser>:<dbpassword>@ds061681.mlab.com:61681/heroku_q0q5mnrs'
 
 mongoose.connect('mongodb://127.0.0.1:27017/uniapp'|| dbUrl, {
     useNewUrlParser: true,
